34519d32df2c386f2e4aa8626afb1aaff5b3639a
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-05-24 18:48:27 -0300

    Category Services + Repository + H2 Database Implemented and Tested with Postman

--- backend/src/main/java/com/carloslaurinedev/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/carloslaurinedev/dscatalog/resources/CategoryResource.java
@@ -1,14 +1,15 @@
 package com.carloslaurinedev.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carloslaurinedev.dscatalog.entities.Category;
+import com.carloslaurinedev.dscatalog.services.CategoryService;
 
 @RestController
 
@@ -16,11 +17,14 @@ import com.carloslaurinedev.dscatalog.entities.Category;
 
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2l, "Electronics"));
+	
+		List <Category> list = service.findAll();
+				
 		return ResponseEntity.ok().body(list);
 	}
 
