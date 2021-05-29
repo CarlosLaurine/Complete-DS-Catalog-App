ac0508099ead6444f06785725e91a237f14a1a92
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-05-29 02:53:12 -0300

    Pageable Request Parameter Refatoration at Product Pagination

--- backend/src/main/java/com/carloslaurinedev/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/carloslaurinedev/dscatalog/services/ProductService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -50,9 +50,9 @@ public class ProductService {
 	}
 
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
 
-		Page<Product> entityPage = repository.findAll(pageRequest);
+		Page<Product> entityPage = repository.findAll(pageable);
 
 		Page<ProductDTO> dtoPage = entityPage.map(entity -> new ProductDTO(entity));
 
