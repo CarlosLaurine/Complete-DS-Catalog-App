33a00dce0348dbf0fb922feb4c3b21a4380a77bc
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-06-07 00:57:21 -0300

    JSON Response Styling for Custom Validation Exception Handling (Exception Handler Method's Logic Updated + Validation Error Class Implemented with a list of MessageError class type)

--- backend/src/main/java/com/carloslaurinedev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/carloslaurinedev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -47,18 +48,24 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(error);
 
 	}
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
 
-		StandardError error = new StandardError();
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		ValidationError error = new ValidationError();
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 
 		error.setTimestamp(Instant.now());
 		error.setStatus(status.value());
-		error.setError("Validation Exception");
+		error.setError("Validation Exception");                                   
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
+
+		for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+
+			error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+
+		}
 
 		return ResponseEntity.status(status).body(error);
 
