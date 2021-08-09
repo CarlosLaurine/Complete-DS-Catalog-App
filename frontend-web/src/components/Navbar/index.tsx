09b2eb47810ea272bf15b032c6732294345b1607
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-08-09 01:45:52 -0300

    Hamburguer Menu at Navbar Created + Styles Implemented

--- frontend-web/src/components/Navbar/index.tsx
+++ frontend-web/src/components/Navbar/index.tsx
@@ -1,18 +1,35 @@
-import './style.css'
+import './style.css';
+import 'bootstrap/js/src/collapse.js';
+
 const Navbar = () => {
   return (
-    <nav className="bg-primary navbar navbar-expand-md main-nav">
+    <nav className="bg-primary navbar navbar-dark navbar-expand-md main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-text-logo">
           <h4>DS Catalog</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div id="dscatalog-navbar" className="collapse navbar-collapse">
           <ul className="main-menu navbar-nav offset-md-2">
             <li>
-              <a href="link" >HOME</a>
+              <a href="link">HOME</a>
             </li>
             <li>
-              <a href="link" className="current">CATALOG</a>
+              <a href="link" className="current">
+                CATALOG
+              </a>
             </li>
             <li>
               <a href="link">ADMIN</a>
