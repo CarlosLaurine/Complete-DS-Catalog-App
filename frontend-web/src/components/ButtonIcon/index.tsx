78a10e02e5f8a34fd3d3954ab3d139efe3275c34
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-08-12 03:00:50 -0300

    Xtra-Small Layout Complete (Following Mobile-First Development Strategy)

---
+++ frontend-web/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const ButtonIcon = () => {
+  return (
+    <button className="button-icon btn btn-primary">
+      <h5>Start your Search now!</h5>
+    </button>
+  );
+};
+
+export default ButtonIcon;
--- frontend-web/src/pages/Home/index.tsx
+++ frontend-web/src/pages/Home/index.tsx
@@ -1,32 +1,27 @@
 import Navbar from 'components/Navbar';
-import { ReactComponent as MainImg } from 'assets/images/main-vectorized-img.svg'
-import './style.css'
+import ButtonIcon from 'components/ButtonIcon';
+import { ReactComponent as MainImg } from 'assets/images/main-vectorized-img.svg';
+import './style.css';
 
 const Home = () => {
-    return (
+  return (
     <>
-        <Navbar/>
-        <div className="home-container">
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Get to Know the Best Online Product Catalog</h1>
+            <p>We'll help you find the Best Products Available at the Market</p>
+            <ButtonIcon />
+          </div>
 
-            <div className="home-card">
-
-                <div className="home-content-container">
-
-                    <h1>Get to Know the Best Online Product Catalog</h1>
-
-                </div>
-
-                <div className="home-image-container">
-
-                    <MainImg/>
-
-                </div>
-
-            </div>    
-
+          <div className="home-image-container">
+            <MainImg />
+          </div>
         </div>
+      </div>
     </>
-    );
-  };
-  
-  export default Home;
+  );
+};
+
+export default Home;
