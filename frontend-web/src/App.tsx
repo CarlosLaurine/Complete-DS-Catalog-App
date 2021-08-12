2d69f3bcdaa22b47a84d819bdb1bcda1d3587c7f
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-08-11 23:12:46 -0300

    Node-sass replaced by Sass + package.json updated + Home Page Initial Structure

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,12 +1,11 @@
-import './assets/styles/custom.scss';
+import 'assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1>Hello DSCatalog</h1>
+      <Home />
     </>
   );
 }
---
+++ frontend-web/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import Navbar from 'components/Navbar';
+import { ReactComponent as MainImg } from 'assets/images/main-vectorized-img.svg'
+
+const Home = () => {
+    return (
+    <>
+        <Navbar/>
+        <div className="home-container">
+
+            <div className="home-card">
+
+                <div className="home-content-container">
+
+                    <h1>Get to Know the Best Online Product Catalog</h1>
+
+                </div>
+
+                <div className="home-image-container">
+
+                    <MainImg/>
+
+                </div>
+
+            </div>    
+
+        </div>
+    </>
+    );
+  };
+  
+  export default Home;
