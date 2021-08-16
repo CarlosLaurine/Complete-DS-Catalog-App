c0b3f98c9423732d5a31ec3cad3959ec5a1cf71a
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-08-15 21:58:34 -0300

    Catalog Page Component Initialized + Product PNG Image Exported from Figma

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,11 +1,12 @@
 import 'assets/styles/custom.scss';
 import './App.css';
 import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
 
 function App() {
   return (
     <>
-      <Home />
+      <Catalog />
     </>
   );
 }
---
+++ frontend-web/src/pages/Catalog/index.tsx
@@ -0,0 +1,14 @@
+import Navbar from 'components/Navbar';
+
+const Catalog = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="my-4 container">
+        <h1>Catalog Screen</h1>
+      </div>
+    </>
+  );
+};
+
+export default Catalog;
