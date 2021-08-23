d80a631e7e8705cb4c51d5e7f2029225e8b63347
Carlos Laurine <cvpinhofsa@gmail.com>
2021-08-23 04:02:06 -0300

    Pagination Bar Style Implemented

--- frontend-web/src/components/Pagbar/index.tsx
+++ frontend-web/src/components/Pagbar/index.tsx
@@ -1,15 +1,16 @@
+import './style.css';
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow-icon.svg';
 
 const Pagbar = () => {
   return (
     <div className="pagination-container">
-      <ArrowIcon />
+      <ArrowIcon className="arrow-previous arrow-inactive" />
       <div className="pagination-item"> 1 </div>
-      <div className="pagination-item"> 2 </div>
+      <div className="pagination-item active"> 2 </div>
       <div className="pagination-item"> 3 </div>
       <div className="pagination-item"> (...) </div>
       <div className="pagination-item"> 10 </div>
-      <ArrowIcon />
+      <ArrowIcon className="arrow-active" />
     </div>
   );
 };
