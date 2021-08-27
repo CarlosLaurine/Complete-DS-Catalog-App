4a89a512f1a2da688fb8bf0c4255c88962a1c808
Carlos Laurine <cvpinhofsa@gmail.com>
2021-08-26 22:54:07 -0300

    Paged and Parameterized Axios Request Implemented at Catalog + Types SpringPage<T> and AxiosParams Created

--- frontend-web/src/pages/Catalog/index.tsx
+++ frontend-web/src/pages/Catalog/index.tsx
@@ -3,24 +3,36 @@ import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import Pagbar from 'components/Pagbar';
+import { useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { useEffect } from 'react';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'util/requests';
+import axios from 'axios';
 
 const Catalog = () => {
-  const product: Product = {
-    id: 3,
-    name: 'Macbook Pro',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 1250.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 3,
-        name: 'Computers',
-      },
-    ],
-  };
+
+  const [springPage, setSpringPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+
+    const axiosParams : AxiosParams = {
+
+      method: 'GET',
+      url : `${BASE_URL}/products`,
+      params: {
+        page : 0,
+        size : 12
+      }
+
+    }
+
+    axios(axiosParams)
+      .then(response => {
+        setSpringPage(response.data);
+      })
+
+  }, [])
 
   return (
     <>
