279f6ea96048b1ed9d54cbdc4f9482e83007735a
Carlos Laurine <cvpinhofsa@gmail.com>
2021-08-27 18:23:11 -0300

    Loader Product Catalog Created and Configured

--- frontend-web/src/pages/Catalog/ProductListLoader/index.tsx
+++ frontend-web/src/pages/Catalog/ProductListLoader/index.tsx
@@ -1,18 +1,21 @@
 import ContentLoader from 'react-content-loader';
+import './style.css'
 
 const ProductListLoader = () => (
-  <ContentLoader
-    speed={2}
-    width={400}
-    height={460}
-    viewBox="0 0 400 460"
-    backgroundColor="#ecebebeb"
-    foregroundColor="#d6d2d2d2"
-  >
-    <rect x="2" y="20" rx="2" ry="2" width="300" height="10" />
-    <rect x="2" y="36" rx="2" ry="2" width="300" height="10" />
-    <rect x="1" y="60" rx="2" ry="2" width="300" height="300" />
-  </ContentLoader>
+  <div className="product-card-loader-container">
+    <ContentLoader
+      speed={2}
+      width={320}
+      height={460}
+      viewBox="0 0 400 460"
+      backgroundColor="#ecebebeb"
+      foregroundColor="#d6d2d2d2"
+    >
+      <rect x="2" y="20" rx="2" ry="2" width="300" height="10" />
+      <rect x="2" y="36" rx="2" ry="2" width="300" height="10" />
+      <rect x="0" y="0" rx="2" ry="2" width="300" height="300" />
+    </ContentLoader>
+  </div>
 );
 
 export default ProductListLoader;
