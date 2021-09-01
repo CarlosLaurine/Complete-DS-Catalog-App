f3efe9193577c9798c53a737dc2f67ee67f4aaf2
Carlos Laurine <cvpinhofsa@gmail.com>
2021-08-31 23:30:06 -0300

    QS Library Added to Project + Login Request Basic Auth Header Const + requestAPILogin Function Created on util/requests.ts

--- frontend-web/src/util/requests.ts
+++ frontend-web/src/util/requests.ts
@@ -1,2 +1,30 @@
+import { getMaxListeners } from 'process';
+import qs from 'qs';
+
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'https://localhost:8080';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
+
+type LoginData = {
+  username: string;
+  password: string;
+};
+
+const basicAuthHeader = () => {
+  return 'Basic' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET);
+};
+
+export const requestAPILogin = (loginData: LoginData) => {
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: basicAuthHeader,
+  };
+
+  const data = qs.stringify({
+    username: 'maria@gmail.com',
+    password: '123456',
+    grant_type: 'password',
+  });
+};
