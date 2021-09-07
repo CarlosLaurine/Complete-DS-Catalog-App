c5edabec7d6a0cfd085cf7e59521725803888b8f
Carlos Laurine <cvpinhofsa@gmail.com>
2021-09-07 04:23:08 -0300

    High Order Function some() Replacement on hasAnyRoles Logic at requests.ts

--- frontend-web/src/util/requests.ts
+++ frontend-web/src/util/requests.ts
@@ -141,12 +141,16 @@ export const hasAnyRoles = (roles: Role[]): boolean => {
   const tokenData = getTokenData();
 
   if (tokenData !== undefined) {
-    for (var i = 0; i < roles.length; i++) {
-      if (tokenData.authorities.includes(roles[i])) {
-        return true;
-      }
-    }
+    return roles.some((role) => tokenData.authorities.includes(role));
   }
+
+  //  if (tokenData !== undefined) {
+  //    for (var i = 0; i < roles.length; i++) {
+  //      if (tokenData.authorities.includes(roles[i])) {
+  //        return true;
+  //      }
+  //    }
+  //  }
 
   return false;
 };
