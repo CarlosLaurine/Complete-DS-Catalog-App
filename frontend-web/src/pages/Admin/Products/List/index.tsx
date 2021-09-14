38450f13b71520656627ca2cbc2c3463edf33fee
Carlos Laurine <cvpinhofsa@gmail.com>
2021-09-14 20:43:24 -0300

    API Product Page Dynamic Rendering on List index.tsx (Consumption with React Hooks + Axios)

--- frontend-web/src/pages/Admin/Products/List/index.tsx
+++ frontend-web/src/pages/Admin/Products/List/index.tsx
@@ -1,28 +1,29 @@
 import './style.css';
 import ProductCardCRUD from 'pages/Admin/Products/ProductCardCRUD';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Product } from 'types/product';
+import { AxiosRequestConfig } from 'axios';
+import { requestAPI } from 'util/requests';
 
 const List = () => {
-  const defaultProduct = {
-    id: 2,
-    name: 'Smart TV',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Books',
+  const [springPage, setSpringPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const axiosParams: AxiosRequestConfig = {
+      method: 'get',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
       },
-      {
-        id: 3,
-        name: 'Computers',
-      },
-    ],
-  };
+    };
+
+    requestAPI(axiosParams).then((response) => {
+      setSpringPage(response.data);
+    });
+  }, []);
 
   return (
     <div className="product-crud-container">
@@ -35,18 +36,13 @@ const List = () => {
         <div className="base-card product-filter-container">Search Bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCardCRUD product={defaultProduct} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCardCRUD product={defaultProduct} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCardCRUD product={defaultProduct} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCardCRUD product={defaultProduct} />
-        </div>
+        {springPage?.content.map((product) => {
+          return (
+            <div className="col-sm-6 col-md-12">
+              <ProductCardCRUD product={product} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
