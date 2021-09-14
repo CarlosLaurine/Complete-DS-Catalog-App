7b4c6e2646bf84a91a36fdb9c180c0447d46a495
Carlos Laurine <cvpinhofsa@gmail.com>
2021-09-13 21:41:07 -0300

    Form Containers and Margins' Stylization Initiated

--- frontend-web/src/pages/Admin/Products/Form/index.tsx
+++ frontend-web/src/pages/Admin/Products/Form/index.tsx
@@ -9,16 +9,26 @@ const Form = () => {
         <form action="">
           <div className="row">
             <div className="col-lg-6">
-              <input className="form-control base-input" type="text" />
-              <input className="form-control base-input" type="text" />
-              <input className="form-control base-input" type="text" />
+              <div className="margin-bottom-30">
+                <input className="form-control base-input" type="text" />
+              </div>
+              <div className="margin-bottom-30">
+                <input className="form-control base-input" type="text" />
+              </div>
+              <div>
+                <input className="form-control base-input" type="text" />
+              </div>
             </div>
             <div className="col-lg-6">
-              <textarea className="form-control base-input" name="" rows={15}></textarea>
+              <textarea
+                className="form-control base-input"
+                name=""
+                rows={15}
+              ></textarea>
             </div>
           </div>
 
-          <div>
+          <div className="product-crud-buttons-container">
             <button className="btn btn-outline-danger">CANCEL</button>
             <button className="btn btn-primary">SAVE</button>
           </div>
