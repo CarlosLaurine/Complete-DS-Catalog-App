83b480085f62bfdc4c281f0d961a4cdd7b7211b9
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-02 21:41:01 -0300

    Product Filter Initial HTML Built + CSS Classname References

--- frontend-web/src/components/ProductFilter/index.tsx
+++ frontend-web/src/components/ProductFilter/index.tsx
@@ -1,7 +1,23 @@
 import './style.css';
 
 const ProductFilter = () => {
-  return <div className="base-card product-filter-container">Search Bar</div>;
+  return (
+    <div className="base-card product-filter-container">
+      <form action="" className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input type="text" className="form-control" />
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <select name="" id="">
+              <option value="">Books</option>
+            </select>
+          </div>
+          <button className="btn btn-outline-secondary">CLEAN</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ProductFilter;
