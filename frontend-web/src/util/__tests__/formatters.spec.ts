28ebbff672ff100ffd6bc05d33cf3e380309bd02
Carlos Pinho <carlos.pinho@ucsal.edu.br>
2021-11-29 20:55:38 -0300

    Describes Created with their Respective Tests for Positive and Non-Positive Number Inputs at formatters.spec.ts

--- frontend-web/src/util/__tests__/formatters.spec.ts
+++ frontend-web/src/util/__tests__/formatters.spec.ts
@@ -1,14 +1,57 @@
 import { formatPrice } from "util/formatters";
 
-test('formatPrice Function should Format Number to pt-BR When 10.1 is Passed as Parameter', () => {
 
-    //ARRANGE
-    const value = 10.1;
+describe("formatPrice for Positive Numbers", () => {
+    test('formatPrice Function should Format Number to pt-BR When 10.1 is Passed as Parameter', () => {
 
-    //ACT
-    const result = formatPrice(value);
+        //ARRANGE
+        const value = 10.1;
+    
+        //ACT
+        const result = formatPrice(value);
+    
+        //ASSERT
+        expect(result).toEqual("10,10");
+    
+    })
+    test('formatPrice Function should Format Number to pt-BR When 0.3 is Passed as Parameter', () => {
 
-    //ASSERT
-    expect(result).toEqual("10,10");
-
+        //ARRANGE
+        const value = 0.3;
+    
+        //ACT
+        const result = formatPrice(value);
+    
+        //ASSERT
+        expect(result).toEqual("0,30");
+    
+    })
 })
+
+describe("formatPrice for Non-Positive Numbers", () => {
+    test('formatPrice Function should Format Number to pt-BR When -15.1 is Passed as Parameter', () => {
+
+        //ARRANGE
+        const value = -15.1;
+    
+        //ACT
+        const result = formatPrice(value);
+    
+        //ASSERT
+        expect(result).toEqual("-15,10");
+    
+    })
+    test('formatPrice Function should Format Number to pt-BR When -0.7 is Passed as Parameter', () => {
+
+        //ARRANGE
+        const value = -0.7;
+    
+        //ACT
+        const result = formatPrice(value);
+    
+        //ASSERT
+        expect(result).toEqual("-0,70");
+    
+    })
+})
+
