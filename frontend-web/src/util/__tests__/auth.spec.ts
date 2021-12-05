ea6115865ebf36187c7e572438f359726bef252c
Carlos Pinho <carlos.pinho@ucsal.edu.br>
2021-12-05 00:35:02 -0300

    Mocking getTokenData Function with JEST SpyOn for hasAnyRoles Unit Test

--- frontend-web/src/util/__tests__/auth.spec.ts
+++ frontend-web/src/util/__tests__/auth.spec.ts
@@ -1,8 +1,24 @@
 import {hasAnyRoles} from '../auth'
+import * as TokenModule from '../token'
 describe("hasAnyRoles Tests", () => {
     test('hasAnyRoles Function should return TRUE When an Empty List is Passed as Parameter', () => {
         //ARRANGE && ACT
         const result = hasAnyRoles([]);
+
+        //ASSERT
+        expect(result).toEqual(true);
+    })
+
+    test('hasAnyRoles Function should return TRUE When User has given Role', () => {
+        //MOCK getTokenData BEHAVIOUR
+        jest.spyOn(TokenModule,"getTokenData").mockReturnValue({
+            exp:0,
+            user_name:"",
+            authorities: ['ROLE_OPERATOR', 'ROLE_ADMIN']
+        })
+        
+        //ARRANGE && ACT
+        const result = hasAnyRoles(["ROLE_ADMIN"]);
 
         //ASSERT
         expect(result).toEqual(true);
